62d00864c7aebf5457ff74be882dd80601fa1d36
Andreas Rutjens <50319647+andreasdevjs@users.noreply.github.com>
2021-09-12 14:00:31 +0200

    working draft

--- index.js
+++ index.js
@@ -1,26 +1,42 @@
+import axios from "axios";
 import  { twitterClient } from './twitterConfig.js';
 import cron from 'node-cron';
 
+
 const fromPriceToPorcentage = (price) => {
-  // convuerte usd en porncetaje
-  return price;
+  return price / 100;
 }
 
 const getBitcoinPrice = async () => {
-  // Instalar axios y hacer llamada.. explicado aqiuí: https://www.coingecko.com/es/api/documentation se puede ver el ejemplo
-  // recibo el precio y lo convierto en el porcentaje
-  // construimos el string del precio.
+  try {
+    const response = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd');
+    const data = await response.data;
+    const price = data.bitcoin.usd;
+    return price;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+const buildTweetString = (porcentage) => {
+  return porcentage.toString();
 }
 
 const makeTweet = async function () {
-  const data = twitterClient.tweets.statusesUpdate({status: 'Hola'});
-  console.log(data);
+  const bitcoinPrice = await getBitcoinPrice();
+  const porcentage = fromPriceToPorcentage(bitcoinPrice)
+  const tweet = buildTweetString(porcentage);
+  try {
+    const result = await twitterClient.tweets.statusesUpdate({status: tweet});
+    console.log('RESULTADO: ', result)
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-
-// Run cron at 16.30 New York timezone
-const task = cron.schedule('30 16 * * *', () => {
-  console.log('running cron');
+// Run cron at 16.30 New York timezone -> 30 16 * * *
+const task = cron.schedule('* * * * *', () => {
+  console.log('Running Tweet Cron');
   makeTweet();
 },{ timezone : "America/New_York" });
 
